d03b62ca2be91024c4e392c0a137119492454fc5
Javier Soler <javier.soler@bbc.co.uk>
2017-11-17 16:04:54 +0000

    [JS] [master] add new line

--- index.js
+++ index.js
@@ -158,6 +158,7 @@ const start = async () => {
     const command = `git commit ${gitOptions.join(' ')} ${message}`;
     const output = await runCommand(command);
 
+    console.log('');
     console.log(chalk.italic.dim.greenBright(output));
     console.log(`🏆  ${chalk.greenBright('Done!')}`);
   } catch (error) {
